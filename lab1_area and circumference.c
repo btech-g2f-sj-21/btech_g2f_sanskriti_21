3cdaa807fdace189242a44618f92d77db394a3ce
btech-g2f-sj-21 <25116cn041@glbitm.ac.in>
2026-02-10 10:13:11 +0530

    Add files via upload

---
+++ lab1_area and circumference.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float n;
+    printf("enter n: ");
+    scanf("%f",&n);
+
+printf("area of circle is: %f",3.14*n*n);
+printf(" \n circumference of circle is: %f",2*3.14*n);
+
+    return 0;
+}
